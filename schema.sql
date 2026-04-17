3c7a3aeb298749b30608e2b936e10b97c76e8c51
Abdullah-Siddiqui02 <siddiquiabdullah040@gmail.com>
2026-04-18 00:55:37 +0530

    added SQL config

--- schema.sql
+++ schema.sql
@@ -1,25 +1,53 @@
 CREATE DATABASE IF NOT EXISTS interview_prep1;
 USE interview_prep1;
 
---  Users Table
 
 CREATE TABLE IF NOT EXISTS users (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     username    VARCHAR(100)  NOT NULL UNIQUE,
-    password    VARCHAR(255)  NOT NULL,           -- stores werkzeug hashed passwords
+    password    VARCHAR(255)  NOT NULL,
     created_at  TIMESTAMP     DEFAULT CURRENT_TIMESTAMP
 );
 
---  Questions Table
 
 CREATE TABLE IF NOT EXISTS questions (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     user_id     INT           NOT NULL,
     title       VARCHAR(255)  NOT NULL,
     topic       VARCHAR(100)  NOT NULL,
-    difficulty  VARCHAR(20)   NOT NULL,           -- Easy / Medium / Hard
+    difficulty  VARCHAR(20)   NOT NULL,           
     created_at  TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS global_problems (
+    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    platform            VARCHAR(50)   NOT NULL,           
+    platform_problem_id VARCHAR(100)  NOT NULL,           
+    title               VARCHAR(255)  NOT NULL,
+    difficulty          VARCHAR(50),                      
+    tags                VARCHAR(255),                     
+    url                 VARCHAR(555)  NOT NULL,
+    description         TEXT,                             
+    created_at          TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(platform, platform_problem_id)
+);
+
+-- User Submissions Table (To track code executions)
+
+CREATE TABLE IF NOT EXISTS user_submissions (
+    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    user_id             INT           NOT NULL,
+    problem_id          INT,                              
+    language            VARCHAR(50)   NOT NULL,
+    code                TEXT          NOT NULL,
+    status              VARCHAR(50)   NOT NULL,           
+    execution_time_ms   FLOAT,
+    memory_kb           FLOAT,
+    created_at          TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (problem_id) REFERENCES global_problems(id) ON DELETE SET NULL
 );
