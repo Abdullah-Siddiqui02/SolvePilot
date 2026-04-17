82f9c045bf06035bfc11d995771f76758d1e92b8
Abdullah-Siddiqui02 <siddiquiabdullah040@gmail.com>
2026-04-18 01:04:10 +0530

    this make dir as package

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,9 @@
-
-
 from routes.auth import auth_bp
 from routes.dashboard import dashboard_bp
 from routes.questions import questions_bp
 from routes.solve import solve_bp
+from routes.problems import problems_bp
+from routes.execute import execute_bp
 
 
 def register_blueprints(app):
@@ -12,3 +12,5 @@ def register_blueprints(app):
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(questions_bp)
     app.register_blueprint(solve_bp)
+    app.register_blueprint(problems_bp)
+    app.register_blueprint(execute_bp)
